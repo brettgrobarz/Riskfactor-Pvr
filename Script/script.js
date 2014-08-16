6bda423a77480b9f61213dc96685adeb42e58f00
brettgrobarz <brettgrobarz@Bretts-MacBook-Pro.local>
2014-08-16 16:47:30 -0500

    saving before another break

--- Script/script.js
+++ Script/script.js
@@ -19,27 +19,35 @@ $(document).ready(function() {
 
 // ethical questions example array 
 
-          var ethicalArray = [['Are you at risk of not getting call back from a job interview based on your overall attractivness?', 'Percent chance an attractive person will receive a callback after an interview 72.32%'],['If you knew would not get caught cheating on your significant other would you do it?', 'Men 74% Women 68%']];
+          var EthicalArray = [['Are you at risk of not getting call back from a job interview based on your overall attractivness?', 'Percent chance an attractive person will receive a callback after an interview 72.32%'],['If you knew would not get caught cheating on your significant other would you do it?', 'Men 74% Women 68%']];
 
 // financial questions example array 
 
-          var financialArray = [['Are you more likely to be homeless as a man?', 'Men are 44% more likely to be homeless'],['Do you typically have enough money on hand to cover one mortgage/rent payment or to purchase one month of groceries?', 'According to Healthways.com, 64% of Americans don’t have enough cash on hand to cover one mortgage payment or to purchase one month of groceries for their families.']];
+          var FinancialArray = [['Are you more likely to be homeless as a man?', 'Men are 44% more likely to be homeless'],['Do you typically have enough money on hand to cover one mortgage/rent payment or to purchase one month of groceries?', 'According to Healthways.com, 64% of Americans don’t have enough cash on hand to cover one mortgage payment or to purchase one month of groceries for their families.']];
 
 // health questions example array 
 
-          var healthArray = [['Do you think males or femals have a higher risk of dying from heat related causes?', '2013: 63% of all heat related fatalities were men. (Men 70-79 are the highest risk)'],['Do you feel it is safe to canoe in Town Lake?', 'Your risk of injuy is roughly 1/10,000']];
+          var HealthArray = [['Do you think males or femals have a higher risk of dying from heat related causes?', '2013: 63% of all heat related fatalities were men. (Men 70-79 are the highest risk)'],['Do you feel it is safe to canoe in Town Lake?', 'Your risk of injuy is roughly 1/10,000']];
 
 // recreation questions example array 
 
-          var recreationArray = [['Do you think you think it is safe to ride a bike in Austin?', 'On Average, 6 people per year die in Austin on their Bikes'],['Do you think it iss safer to drive a motorcycle during the day or at night? ', '60% of motorcycle accidents occur during the daytime']];
+          var RecreationArray = [['Do you think you think it is safe to ride a bike in Austin?', 'On Average, 6 people per year die in Austin on their Bikes'],['Do you think it iss safer to drive a motorcycle during the day or at night? ', '60% of motorcycle accidents occur during the daytime']];
 
 // saftey questions example array 
 
-          var safteyArray = [['Is it risky to chew on a ball point pen?', 'On average, 100 people choke to death on ballpoint pens every year'],['Do you think sleeping on a pillow is dangerous?', 'More than 6,000 people with pillow-related injuries check into U.S. emergency rooms every year.']];
+          var SafteyArray = [['Is it risky to chew on a ball point pen?', 'On average, 100 people choke to death on ballpoint pens every year'],['Do you think sleeping on a pillow is dangerous?', 'More than 6,000 people with pillow-related injuries check into U.S. emergency rooms every year.']];
 
 // function to populate and loop through questions on each page
 
-  function questionPopulate (questions){
+
+// create an if then function that checks if the ID is equal to a certain id. if it is, then we want to call that array and loop through it 
+
+
+          $('.p-question').replaceWith('<p class="p-question">' + id + '</p>');
+
+// function to populate and loop through questions on each page
+
+  // function questionPopulate (questions){
 
           // for (var i = id+'Array'.length - 1; i >= 0; i--) {
           //   id+'Array'[i]
@@ -49,11 +57,15 @@ $(document).ready(function() {
         // $(".p-question").replaceWith(id);
 
           // };
+// '<div class="row">
+//             <div class="button-container column large-6 column medium-6">
+//               <a class="button">Men</a>
+//               <a class="button">Women</a>
+//             </div>'
 
 
 
 
-};
 });
 });
     
